11a689d816d66fe53defba1ecb2dfa1704a57f68
Daniel <danielbellinijf21@gmail.com>
2022-09-15 20:48:29 -0300

    Basic Layout of Topbar

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,35 +1,63 @@
-import styled from "styled-components";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import "./style.css"
 
 export default function Topbar (){
     return(
-        <Container>  
+        <Conteiner> 
             <div>
-                <h1>W-Cup Store</h1>
+                <Link to={"/"}>
+                    <div className="Logo">
+                        <h1>W-Cup Store</h1>   
+                    </div>
+                </Link>
+                <Link to={"/login"}>
+                    <div>
+                        <p>Usuario/Login</p>
+                    </div>
+                </Link>
+                <Link to ={"/cart"}>
+                    <div>
+                        <ion-icon name="cart-outline"></ion-icon>    
+                    </div>      
+                </Link>
             </div>
-            <div class="pesquisa">
+            <div className="search">
                 <input type="text" placeholder="Pesquisar" />
             </div>
             <div>
-                <p>Usuario/Login</p>
+                <div className="button"><p>Brasil</p></div>
+                <div className="button"><p>Portugal</p></div>
+                <div className="button"><p>Argentina</p></div>
+                <div className="button"><p>Alemanha</p></div>
             </div>
-            <div>
-                <ion-icon name="cart-outline"></ion-icon>    
-            </div>         
-        </Container>
+        </Conteiner>
     )
 }
-const Container = styled.div`
-    width:100%;
-    height:60px;
 
-    display:flex;
-    align-content: center;
-    align-items: center;
-    justify-content: space-evenly;
+const Conteiner = styled.div`
+position: fixed;
+left: 0;
+top: 0;
+z-index: 1;
 
+width:100%;
 
-    background-color:#84D904;
+display:flex;
+flex-direction: column;
+align-items: center
+padding-top:10px;
+
+background-color:#84D904;
+
+    div{
+        height:40px;
+        width: 100%;
+        max-width:800px;
+        display:flex;
+        align-content: center;
+        align-items: center;
+        justify-content: space-evenly;
+    }
 
 `
