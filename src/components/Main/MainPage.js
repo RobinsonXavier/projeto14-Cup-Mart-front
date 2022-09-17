f9eedf6c70352a0342c2f85095e0158d2dc96041
Robinson_Xavier <robinson.webzen@gmail.com>
2022-09-16 21:05:30 -0300

    added slides

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,5 +1,26 @@
+import React from 'react';
+import axios from "axios";
+
+import styled from "styled-components";
+
+import Slideshow from '../Slideshow/Slideshow';
+
 export default function MainPage (){
     return(
-        <></>
+        <>
+         <MainPageApp>
+            <Slideshow />
+         </MainPageApp>
+        </>
     )
 }
+
+const MainPageApp = styled.div `
+    margin-top: 50px;
+    width: 85vw;
+    background-color: blue;
+`;
+
+const slides = styled.div `
+
+`;
---
+++ src/components/Slideshow/Slideshow.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from "styled-components";
+import {Fade} from 'react-slideshow-image';
+
+import 'react-slideshow-image/dist/styles.css';
+
+import cheer from '../../assets/images/cheerTeam.jpg';
+import wCupProgram from '../../assets/images/programWCup.jpg';
+
+export default function Slideshow() {
+    return(
+        <>
+            <SliderGroup>
+                <Fade>
+                    <SlidePage>
+                        <img src={cheer} alt='' />
+                        <span>Austria surpreende no jogo contra Argentina</span>
+                        <DegradeDiv>
+                        </DegradeDiv>
+                    </SlidePage> 
+                    <SlidePage>
+                        <img src={wCupProgram} alt='' />
+                        <span>Confira a programação da copa e não perca o jogo do seu time</span>
+                        <DegradeDiv>
+                        </DegradeDiv>
+                    </SlidePage>         
+                </Fade>
+            </SliderGroup>
+        </>
+    )
+};
+
+const SliderGroup = styled.div `
+    width: 500px;
+    position: relative;
+`;
+
+const SlidePage = styled.div `
+    box-sizing: border-box;
+    border-radius: 5px;
+    img {
+        box-sizing: border-box;
+        position: relative;
+        max-width: 100%;
+        border-radius: 5px;
+    }
+
+    span {
+        position: absolute;
+        color: #B3C8F2;
+        bottom: 10px;
+        left: 5px;
+        font-weight: 700;
+        font-size: 22px;
+        -webkit-text-stroke: 0.5px #6BB3F2;
+        text-decoration: double;
+        z-index: 2;
+    }
+    
+`;
+
+const DegradeDiv = styled.div `
+    position: absolute;
+    box-sizing: border-box;
+    border-radius: 5px;
+    width: 500px;
+    height: 100%;
+    top: 0px;
+    background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+    z-index: 1;
+
+    span {
+        position: absolute;
+        color: #B3C8F2;
+        bottom: 10px;
+        left: 5px;
+        font-weight: 700;
+        font-size: 22px;
+        -webkit-text-stroke: 0.5px #6BB3F2;
+        text-decoration: double;
+    }
+`;
